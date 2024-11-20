8f052f85491b1bad98b3bdc337885bd99f646724
k19950907 <k19950907@gmail.com>
2024-11-20 21:31:41 +0800

    router init

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require("express")
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("hello world")
-})
-
 app.get("/todos", (req, res)  => {
   res.send("get all todos")
 })
