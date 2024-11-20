85050e70cd67f79da919cd9188794ea27b212662
k19950907 <k19950907@gmail.com>
2024-11-20 21:28:41 +0800

    router init

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ app.get("/", (req, res) => {
   res.send("hello world")
 })
 
+app.get("/todos", (req, res)  => {
+  res.send("get all todos")
+})
+
+app.get("/todos/new", (req, res) => {
+  res.send("creat todo")
+})
+
+app.post("/todos", (req, res) => {
+  res.send("add todo")
+})
+
+app.get("/todos/:id", (req, res)=> {
+  res.send(`get todo: ${req.params.id}`)
+})
+
+app.get("/todos/:id/edit", (req, res) => {
+  res.send(`get todo edit: ${req.params.id}`)
+})
+
+app.put("/todos/:id", (req, res) => {
+  res.send("modify todo")
+})
+
+app.delete("/todos/:id", (res, req) => {
+  res.send("delete todo")
+})
+
 const port = 3000
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
