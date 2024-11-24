ba470145f1439f0d1274841e69fe573f742c21ab
k19950907 <k19950907@gmail.com>
2024-11-25 07:12:16 +0800

    initialize handlebars layout

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const app = express()
+
+const { engine } = require("express-handlebars")
+
 const port = 3000
 
 const db = require("./models")
 const Todo = db.Todo
+
+app.engine(".hbs", engine({extname: ".hbs"}))
+app.set("view engine", ".hbs")
+app.set("views", "./views")
+
+app.get("/", (req, res) => {
+  res.render("index")
+})
 
 app.get("/todos", (req, res)  => {
   return Todo.findAll()
