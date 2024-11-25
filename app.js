dff792f1ce47bd2787a4c76ec21157999be50e7d
k19950907 <k19950907@gmail.com>
2024-11-26 00:38:03 +0800

    implement updating todo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 
 const { engine } = require("express-handlebars")
+const methodOverride = require("method-override")
 
 const port = 3000
 
@@ -13,6 +14,7 @@ app.set("view engine", ".hbs")
 app.set("views", "./views")
 
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
 
 app.get("/", (req, res) => {
   res.render("index")
@@ -39,6 +41,7 @@ app.post("/todos", (req, res) => {
 
 app.get("/todos/:id", (req, res)=> {
   const id = req.params.id
+
   return Todo.findByPk(id, {
     attributes: ["id", "name"],
     raw: true
@@ -47,11 +50,21 @@ app.get("/todos/:id", (req, res)=> {
 })
 
 app.get("/todos/:id/edit", (req, res) => {
-  res.send(`get todo edit: ${req.params.id}`)
+  const id = req.params.id
+
+  return Todo.findByPk(id, {
+    attributes: ["id", "name"],
+    raw: true
+  })
+    .then((todo) => res.render("edit", {todo}))
 })
 
 app.put("/todos/:id", (req, res) => {
-  res.send("modify todo")
+  const body = req.body
+  const id = req.params.id
+
+  return Todo.update({name: body.name}, {where: {id}})
+    .then(() => res.redirect(`/todos/${id}`))
 })
 
 app.delete("/todos/:id", (res, req) => {
