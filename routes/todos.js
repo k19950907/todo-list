14da0cf6412b2baeb5217aded2c79968e55c0e1d
k19950907 <k19950907@gmail.com>
2024-12-15 01:12:14 +0800

    add pagination

--- routes/todos.js
+++ routes/todos.js
@@ -5,92 +5,97 @@ const db = require("../models")
 const Todo = db.Todo
 
 router.get("/", (req, res, next) => {
-    return Todo.findAll({
-      attributes: ["id", "name", "isComplete"],
-      raw: true
-    })
-      .then((todos) => res.render("todos", { todos, error: req.flash("error") }))
-      .catch((error) => {
-        error.errorMessage = "資料取得失敗"
-        next(error)
-    })
+  const page = parseInt(req.query.page) || 1
+  const limit = 10
+
+  return Todo.findAll({
+    attributes: ["id", "name", "isComplete"],
+    raw: true
+  })
+    .then((todos) => res.render("todos", {
+      todos: todos.slice((page-1) * limit, page*limit),
+      prev: page > 1 ? page - 1: page,
+      next: page+1,
+      page
+    }))
+    .catch((error) => {
+      error.errorMessage = "資料取得失敗"
+      next(error)
+  })
 })
 
 router.get("/new", (req, res, next) => {
-    return res.render("new")
+  return res.render("new")
 })
 
 router.post("/", (req, res, next) => {
-    const name = req.body.name
+  const name = req.body.name
 
-    return Todo.create({ name })
-      .then(() => {
-        req.flash("success", "新增成功")
-        return res.redirect("/todos")
-      })
-      .catch((error) => {
-        error.errorMessage = "新增失敗"
-        next(error)
-      })
+  return Todo.create({ name })
+    .then(() => {
+      req.flash("success", "新增成功")
+      return res.redirect("/todos")
+    })
+    .catch((error) => {
+      error.errorMessage = "新增失敗"
+      next(error)
+    })
   })
 
 router.get("/:id", (req, res, next) => {
-    const id = req.params.id
+  const id = req.params.id
 
-    return Todo.findByPk(id, {
-      attributes: ["id", "name", "isComplete"],
-      raw: true
-    })
-      .then((todo) => res.render("todo", { todo }))
-      .catch((error) => {
-        error.errorMessage = "資料取得失敗"
-        next(error)
-      })
+  return Todo.findByPk(id, {
+    attributes: ["id", "name", "isComplete"],
+    raw: true
+  })
+    .then((todo) => res.render("todo", { todo }))
+    .catch((error) => {
+      error.errorMessage = "資料取得失敗"
+      next(error)
+  })
 })
 
 router.get("/:id/edit", (req, res, next) => {
-    const id = req.params.id
+  const id = req.params.id
 
-    return Todo.findByPk(id, {
-      attributes: ["id", "name", "isComplete"],
-      raw: true
-    })
-      .then((todo) => res.render("edit", { todo, error: req.flash("error") }))
-      .catch((error) => {
-        
-        error.errorMessage = "資料取得失敗"
-        next(error)
-      })
+  return Todo.findByPk(id, {
+    attributes: ["id", "name", "isComplete"],
+    raw: true
+  })
+  .then((todo) => res.render("edit", { todo, error: req.flash("error") }))
+  .catch((error) => {
+    error.errorMessage = "資料取得失敗"
+    next(error)
+  })
 })
 
 router.put("/:id", (req, res, next) => {
-    const { name, isComplete } = req.body
-    const id = req.params.id
+  const { name, isComplete } = req.body
+  const id = req.params.id
 
-    return Todo.update({ name, isComplete: isComplete === "completed" }, { where: { id } })
-      .then(() => {
-        req.flash("success", "編輯成功")
-        return res.redirect(`/todos/${id}`)
-      })
-      .catch((error) => {
-        
-        error.errorMessage ="編輯失敗"
-        next(error)
-      })
+  return Todo.update({ name, isComplete: isComplete === "completed" }, { where: { id } })
+    .then(() => {
+      req.flash("success", "編輯成功")
+      return res.redirect(`/todos/${id}`)
+    })
+    .catch((error) => {
+      error.errorMessage ="編輯失敗"
+      next(error)
+    })
 })
 
 router.delete("/:id", (req, res, next) => {
-    const id = req.params.id
-    return Todo.destroy({ where: { id } })
-      .then(() => {
-        req.flash("success", "刪除成功")
-        return res.redirect("/todos")
-      })
-      .catch((error) => {
-        
-        error.errorMessage = "刪除失敗"
-        next(error)
-      })
+  const id = req.params.id
+  return Todo.destroy({ where: { id } })
+    .then(() => {
+      req.flash("success", "刪除成功")
+      return res.redirect("/todos")
+    })
+    .catch((error) => {
+      error.errorMessage = "刪除失敗"
+      next(error)
+    })
 })
 
 module.exports = router
